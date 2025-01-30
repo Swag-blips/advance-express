d91d42545cf6a8eb2b4ee8f36728f12fd522c757
Swag-blips <coderblip@gmail.com>
2025-01-30 09:22:32 +0100

    api versioning, customMiddleware errorHandlers done

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ dotenv.config();
 import { globalErrorhandler } from "./middleware/errorHandler.js";
 import { urlVersioning } from "./middleware/apiVersioning.js";
 import { createBasicLimiter } from "./middleware/rateLimiting.js";
+import itemRoutes from "./routes/item.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(configureCors());
-app.use(createBasicLimiter(100, 15 * 60 * 1000));
+app.use(createBasicLimiter(2, 15 * 60 * 1000));
 app.use(requestLogger);
 app.use(addTimeStamp);
-app.use("/api/v1", urlVersioning("v1"));
+app.use(urlVersioning("v1"));
+app.use("/api/v1", itemRoutes);
 app.use(globalErrorhandler);
 
 app.listen(PORT, () => {
