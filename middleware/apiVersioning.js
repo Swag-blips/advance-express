9444f7a9e6276c1936276363cbc8914f4acd8461
Swag-blips <coderblip@gmail.com>
2025-01-30 08:23:46 +0100

    feat:api versioning and header verisoning

--- middleware/apiVersioning.js
+++ middleware/apiVersioning.js
@@ -8,3 +8,32 @@ const urlVersioning = (version) => (req, res, next) => {
     });
   }
 };
+
+const headerVersioning = (version) => (req, res, next) => {
+  if (req.get("Accept-Version") === version) {
+    next();
+  } else {
+    res.status(404).json({
+      success: false,
+      error: "API version is not supported",
+    });
+  }
+};
+
+const contentTypeVersioning = (version) => (req, res, next) => {
+  const contentType = req.get("Content-Type");
+
+  if (
+    contentType &&
+    contentType.includes(`application/vnd.api.${version}+json`)
+  ) {
+    next();
+  } else {
+    res.status(404).json({
+      success: false,
+      error: "API version is not supported",
+    });
+  }
+};
+
+export { contentTypeVersioning, headerVersioning, urlVersioning };
--- server.js
+++ server.js
@@ -3,11 +3,8 @@ import express from "express";
 import configureCors from "./config/cors.config.js";
 import { addTimeStamp, requestLogger } from "./middleware/customMiddleware.js";
 dotenv.config();
-import {
-  APIError,
-  asyncHandler,
-  globalErrorhandler,
-} from "./middleware/errorHandler.js";
+import { globalErrorhandler } from "./middleware/errorHandler.js";
+import { urlVersioning } from "./middleware/apiVersioning.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,8 +13,8 @@ app.use(express.json());
 app.use(configureCors());
 app.use(requestLogger);
 app.use(addTimeStamp);
-
-app.use(globalErrorhandler)
+app.use("/api/v1", urlVersioning("v1"));
+app.use(globalErrorhandler);
 
 app.listen(PORT, () => {
   console.log(`server is now running on port ${PORT}`);
