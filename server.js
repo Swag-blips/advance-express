94df7d70dd5850bd680da5978ea8c4ac78bfb9a1
Swag-blips <coderblip@gmail.com>
2025-01-30 08:36:43 +0100

    feat:express rate limit

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ import { addTimeStamp, requestLogger } from "./middleware/customMiddleware.js";
 dotenv.config();
 import { globalErrorhandler } from "./middleware/errorHandler.js";
 import { urlVersioning } from "./middleware/apiVersioning.js";
+import { createBasicLimiter } from "./middleware/rateLimiting.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(configureCors());
+app.use(createBasicLimiter(100, 15 * 60 * 1000));
 app.use(requestLogger);
 app.use(addTimeStamp);
 app.use("/api/v1", urlVersioning("v1"));
