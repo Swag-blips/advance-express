2d8d9f49957df73fa4d731f216dd933997b71d40
Swag-blips <coderblip@gmail.com>
2025-02-01 16:59:17 +0100

    feat:io redis implementation

---
+++ redis/io-redis.js
@@ -0,0 +1,20 @@
+import { Redis } from "ioredis";
+// redis client library for nodejs
+
+const redis = new Redis();
+
+async function ioRedis() {
+  try {
+    await redis.set("key", "value");
+
+    const value = await redis.get("key");
+
+    console.log(value);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    redis.quit();
+  }
+}
+
+ioRedis();
