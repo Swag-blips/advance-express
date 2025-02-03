b3b37d66dec3801a91452e97c224ff9b4c814d7e
Swag-blips <coderblip@gmail.com>
2025-02-03 19:57:27 +0100

    feat:create post controller

---
+++ social-media-microservice/post-service/src/controllers/post-controller.js
@@ -0,0 +1,48 @@
+import Post from "../models/Post.js";
+import logger from "../utils/logger.js";
+
+const createPost = async (req, res) => {
+  try {
+    const { content, mediaIds } = req.body;
+
+    const newPost = new Post({
+      user: req.user.userId,
+      content,
+      mediaIds: mediaIds || [],
+    });
+
+    await newPost.save();
+    logger.info("Post created successfully");
+    res
+      .status(201)
+      .json({ success: true, message: "Post created successfully" });
+  } catch (error) {
+    logger.error("error creating post", error);
+    res.status(500).json({
+      success: false,
+      message: "Error creating post",
+    });
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  try {
+  } catch (error) {
+    logger.error("error fetching posts", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching post",
+    });
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+  } catch (error) {
+    logger.error("error fetching post", error);
+    res.status(500).json({
+      success: false,
+      message: "Error fetching post by ID",
+    });
+  }
+};
