6a40bc6a1935eba09f363e68dee6123ac18773f3
Swag-blips <coderblip@gmail.com>
2025-02-15 09:01:07 +0100

    feat:rabbitmq amqlib setup

---
+++ social-media-microservice/post-service/src/utils/rabbitmq.js
@@ -0,0 +1,23 @@
+import amqp from "amqplib";
+import logger from "./logger";
+
+let connection = null;
+let channel = null;
+
+const EXCHANGE_NAME = "facebook_event";
+
+async function connectRabbitMQ() {
+  try {
+    connection = await amqp.connect(process.env.RABBITMQ_URL);
+    channel = await connection.createChannel();
+
+    await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
+    logger.info("Connected to rabbit mq");
+
+    return channel;
+  } catch (error) {
+    logger.error("Error connecting to rabbit MQ");
+  }
+}
+
+export default connectRabbitMQ;
