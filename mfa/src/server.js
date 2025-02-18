7f0df7481857106fee4e3490fa0e02b2de575dbd
Swag-blips <coderblip@gmail.com>
2025-02-18 07:29:13 +0100

    feat:mongodb setup and mongoose connect

---
+++ mfa/src/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+import dbConnect from "./config/dbConnect.js";
+dotenv.config();
+
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cors(corsOptions));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+// routes
+
+// listen to app
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  dbConnect();
+});
